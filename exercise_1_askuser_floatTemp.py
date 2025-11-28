e346a9b8bfb59b14e09d3751b12bf7acbed282aa
Mercy Wanjiku <mk7693964@gmail.com>
2025-11-28 17:53:27 +0300

    exercises day one

--- exercise_1_askuser_floatTemp.py
+++ exercise_1_askuser_floatTemp.py
@@ -1,6 +1,7 @@
 
 def main():
-
+    float_temp = float(input("What is your temperature in degress?  "))
+    print(f"The temperature in F is: {float_temp * 9/5 + 32}")
 
 
 if __name__ == '__main__':
