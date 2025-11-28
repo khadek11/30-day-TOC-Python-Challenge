cb84bcbaee4abeb6a3bed612e048e2d98815ea23
Mercy Wanjiku <mk7693964@gmail.com>
2025-11-28 18:37:40 +0300

    squares and cubes

---
+++ exercise_2_askNumber_squarecude.py
@@ -0,0 +1,19 @@
+
+def isValid(num):
+    while True:
+        user = input(num)
+        try:
+            return int(user)
+        except ValueError:
+            print("Please enter a valid integer.")
+
+
+def main():
+    integer = isValid("Enter an integer: ")
+    square_integer = integer ** 2
+    cube_integer = integer ** 3
+    print(f'The square of integer is {square_integer} and the cube of integer is {cube_integer}')
+
+
+if __name__ == '__main__':
+    main()
