6c4ae334c958935208f449c676d0d869506a73ff
Mercy Wanjiku <mk7693964@gmail.com>
2025-12-22 21:45:33 +0300

    day 23 to 27 exercises

--- exercise_3_askuser_floatTemp.py
+++ exercise_3_askuser_floatTemp.py
@@ -1,7 +1,13 @@
 import sys
 def main():
-    float_temp = float(input("What is your temperature in degress?  "))
-    print(f"The temperature in F is: {float_temp * 9/5 + 32}")
+    # Ask the user for input and convert it to a float
+    celsius = float(input("Enter temperature in °C: "))
+
+    # Perform the conversion
+    fahrenheit = celsius * 9 / 5 + 32
+
+    # Print the result using an f-string
+    print(f"{celsius}°C is equal to {fahrenheit}°F")
 
 
 if __name__ == '__main__':
